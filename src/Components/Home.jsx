872dd12e63174ff210bfe93277841c366e76301e
kibet nathan <nathanrufus540@gmail.com>
2024-02-19 11:18:04 +0300

    Home and navbar part

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Home() {
+	return <div className=" md:px-12 p-4 max-w-screen-2xl mx-auto mt-28">
+        <div>
+            <div>
+                {/* banner content */}
+                <div>
+                    <h2>Develop your skills without diligence</h2>
+                    <p>A good example of a paragraph contains a topic sentence, details and a conclusion. There are many different kinds of animals that live in china.</p>
+                    <div> <button>Get started</button><button>Discount</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Home
